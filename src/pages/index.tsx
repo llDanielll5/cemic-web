49893ad6b494a32afb6e5fdf37c4f6c7626f49f8
Daniel Mota <danixmotaa7x@gmail.com>
2023-02-26 16:08:11 -0300

    update meta tag for google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,7 +109,12 @@ Gostaria de realizar o agendamento para conhecer melhor o projeto social que a C
           name="description"
           content="Centro Médico e de Implantes Comunitário"
         />
+        <meta name="author" content="Sofx Softwares Inteligentes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keywords"
+          content="cemic, cemicdf, implantes, implante comunitario, brasilia, cemic brasilia, ong, ongcemic, ong brasilia, ong dentes, dentes, protese, ortodontia"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
