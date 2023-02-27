0cee39ef5b65c7c07a2e1ee4134f0c98ae8b3737
Daniel Mota <danixmotaa7x@gmail.com>
2023-02-27 02:34:29 -0300

    dashboard add pagination

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,22 +1,65 @@
 /* eslint-disable @next/next/no-img-element */
+import React, { useState, useCallback, useEffect } from "react";
 import { dashboardNav } from "data";
-import React from "react";
 import styles from "../../styles/Dashboard.module.css";
 
 const Dashboard = () => {
+  const [page, setPage] = useState("Geral");
+
+  const getActualPageSelected = useCallback(() => {
+    const list = document?.querySelectorAll("li");
+    const setSelectedItem = (pageIndex: number, index: number) => {
+      if (index === pageIndex) {
+        list[pageIndex].style?.setProperty("background", "white");
+        list[pageIndex]
+          .querySelector("a")
+          ?.style.setProperty("color", "#09aae8");
+      } else {
+        list[index].style?.setProperty("background", "#09aae8");
+        list[index].querySelector("a")?.style.setProperty("color", "white");
+      }
+    };
+    if (page === "Geral") {
+      list?.forEach((i, index) => {
+        setSelectedItem(1, index);
+      });
+    } else if (page === "Pacientes") {
+      list?.forEach((i, index) => {
+        setSelectedItem(2, index);
+      });
+    } else if (page === "Fechamentos") {
+      list?.forEach((i, index) => {
+        setSelectedItem(3, index);
+      });
+    } else if (page === "Doações") {
+      list?.forEach((i, index) => {
+        setSelectedItem(4, index);
+      });
+    } else if (page === "Dentistas") {
+      list?.forEach((i, index) => {
+        setSelectedItem(5, index);
+      });
+    }
+  }, [page]);
+
+  useEffect(() => {
+    getActualPageSelected();
+  }, [getActualPageSelected]);
+
   const cemicLogo = (props: any) => (
     <li>
       <a href={props.href}>
         <img
-          src="/images/cemicLogo.png"
+          src="/images/logo.png"
           alt="logo"
           className={styles["cemic-logo"]}
         />
+        <span className={styles.title}>{"Daniel Mota"}</span>
       </a>
     </li>
   );
   const navigationRender = (props: any) => (
-    <li>
+    <li onClick={() => setPage(props.title)}>
       <a>
         <span className={styles.icon}>{props.icon}</span>
         <span className={styles.title}>{props.title}</span>
