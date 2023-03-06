3dcc354c8f9c7f3af922b88ccf3f7c1da7575c4a
Daniel Mota <danixmotaa7x@gmail.com>
2023-03-06 01:05:36 -0300

    update router query clients

--- src/pages/patient/[userid].tsx
+++ src/pages/patient/[userid].tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const PatientScreen = () => {
   const router = useRouter();
-  const userid = router?.query?.userid;
+  const userid = router.query.userid;
   return <div />;
 };
 
