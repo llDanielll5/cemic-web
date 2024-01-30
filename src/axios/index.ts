a063f61b874a73cc1d9534cedf5b4e819d9e0028
Daniel Mota <danixmotaa7x@gmail.com>
2024-01-30 14:48:51 -0300

    update to enviroment variables

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -4,7 +4,6 @@ import { getCookie } from "cookies-next";
 export const serverUrl = process.env.DEV_SERVER_URL;
 export const jwt = getCookie("jwt");
 export const headerAuth = { headers: { Authorization: `Bearer ${jwt}` } };
-
 // ----------------------------------------------------------------------
 
 const axiosInstance = axios.create({ baseURL: serverUrl });
